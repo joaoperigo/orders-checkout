f10d98f1113ebc0d79ff673ad086ffaee538fdbb
unknown <joperigo@gmail.com>
2022-03-26 21:29:44 -0300

    Get method for AllProductsList created

--- app-devio/src/components/AllProductsList.vue
+++ app-devio/src/components/AllProductsList.vue
@@ -2,35 +2,21 @@
 
 <form action="" @submit="sendForm($event)">
 
-    <div class="mb-3">
-        <h2>#01 | Nome produto</h2>
-        <img src="http://placekitten.com/200/200" alt="">
-    </div>
-
-    <div class="mb-3">
-        <label for="vegano" class="form-label">Email address</label>
-        <select class="form-select" aria-label="Default select example" v-model="restricao">
-            <option selected>Selecione restrição alimentar</option>
-            <option value="1">Vegano (nada de animal)</option>
-            <option value="2">Onívoro (carnes e vegetais)</option>
-            <option value="3">Vegetariano (come queijo e ovo)</option>
-        </select>
-    </div>
-
-    <div class="mb-3">
-        <div class="form-check">
-            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" v-model="detalhes">
-            <label class="form-check-label" for="flexCheckDefault">
-                Default checkbox
-            </label>
+    <div class="item-menu" v-for="produto in produtos" :key="produto.id">
+        <div class="mb-3">
+            <h2>#{{produto.id}} | {{produto.titulo}}</h2>
+            <img :src="produto.img" :alt="produto.alt">
         </div>
-    </div>
-
-    <div class="mb-3">
-        <label for="observacoes" class="form-label">Email address</label>
-        <div class="form-floating">
-            <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea" v-model="observacoes"></textarea>
-            <label for="floatingTextarea">Observações</label>
+        <div class="mb-3">
+            <label for="observacoes" class="form-label">Email address</label>
+            <div class="form-floating">
+                <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea" v-model="observacoes"></textarea>
+                <label for="floatingTextarea">Observações</label>
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="usr">QTD:</label>
+            <input type="number" id="usr" name="quantity" min="1" max="5">
         </div>
     </div>
 
@@ -45,21 +31,27 @@ export default {
     name: 'AllProductsList',
     data() {
         return {
-            restricao: null,
-            observacoes: null
+            produtos: null,
+            produto_id: null,
+            observacoes: null,
+            qtd: null
         }
     },
     methods: {
+        async getProdutos() {
+            const req = await fetch("http://localhost:3000/produtos")
+            const data = await req.json()
+
+            this.produtos = data
+        },
         sendForm(e) {
             e.preventDefault()
-
-            const restricao = this.restricao
-            const observacoes = this.observacoes
-            
-            console.log(this.restricao)
-            console.log(this.detalhes)
+            const observacoes = this.observacoes 
             console.log(this.observacoes)
         }
+    },
+    mounted() {
+        this.getProdutos()
     }
 }
 </script>
