09ea82017e7593b0a21400abc041fdf5010ef676
unknown <joperigo@gmail.com>
2022-03-28 12:40:21 -0300

    Accordion bootstrap set to vue in allProductsList

--- app-devio/src/components/AllProductsList.vue
+++ app-devio/src/components/AllProductsList.vue
@@ -2,18 +2,31 @@
 {{produtoPedido}}
 <form action="" @submit="pagar($event, 4)">
 
-    <div class="item-menu" v-for="produto in produtos" :key="produto.id">
-        <div class="mb-3">
-            <h2>#{{1+produto.id}} | {{produto.titulo}}</h2>
-            <img :src="produto.img" :alt="produto.alt">
-        </div>
-        <div class="form-group">
-            <label for="usr">QTD:</label>
-            <input type="number" id="usr" name="quantity" min="0" max="25" v-model="produto.qtd" @input="qtdProduto(produto.id, produto.qtd)">
-            <h1>{{produto.qtd}} | {{produto.valor}}</h1>
-        </div>
-        <div>
-            {{produto.valor}}
+    <div class="accordion" id="lista-produtos">
+        <div class="accordion-item" v-for="produto in produtos" :key="produto.id">
+
+            <h2 class="accordion-header" :id="`heading${produto.id}`">
+                <button class="accordion-button" type="button" data-bs-toggle="collapse" :data-bs-target="`#collapse${produto.id}`" aria-expanded="true" :aria-controls="`collapse${produto.id}`">
+                    <span>
+                        #{{1+produto.id}} | {{produto.titulo}}
+                    </span>
+                    <img :src="produto.img" :alt="produto.alt">
+                </button>
+            </h2>
+
+            <div :id="`collapse${produto.id}`" class="accordion-collapse collapse" :aria-labelledby="`heading${produto.id}`">
+                <div class="accordion-body">
+                    <div class="form-group">
+                        <label for="usr">QTD:</label>
+                        <input type="number" id="usr" name="quantity" min="0" max="25" v-model="produto.qtd" @input="qtdProduto(produto.id, produto.qtd)">
+                        <h1>{{produto.qtd}} | {{produto.valor}}</h1>
+                    </div>
+                    <div>
+                        {{produto.valor}}
+                    </div>
+                </div>
+            </div>
+
         </div>
     </div>
 
